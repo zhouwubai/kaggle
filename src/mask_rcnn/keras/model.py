76ca95e3d5db9afe1c5045c9fcbab9188dba1ca6
Wubai Zhou <wubai.zhou@uber.com>
2018-04-19 23:26:44 -0700

    add identity block

--- src/mask_rcnn/keras/model.py
+++ src/mask_rcnn/keras/model.py
@@ -93,9 +93,47 @@ def identity_block(input_tensor, kernel_size, filters, stage, block,
     The identy_block is the block that has no conv layer at shortcut
 
     Args:
-
+        input_tensor: input tensor
+        kernel_size: default 3, the kernel size of middle conv layer at main
+            path
+        filters: list of integers, the nb_filters of 3 conv layer at main path
+        stage: integer, current stage label, used for generating layer names
+        block: 'a', 'b' ..., current block label, used for layer names
+        use_bias: Boolean. To use or not use a bias in conv layers
+        train_bn: Boolean. Train or freeze Batch Norm Layers
     """
+    nb_filter1, nb_filter2, nb_filter3 = filters
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
 
+    x = KL.Conv2D(nb_filter1, (1, 1), name=conv_name_base + '2a',
+                  use_bias=use_bias)(input_tensor)
+    x = BatchNorm(name=bn_name_base + '2a')(x, training=train_bn)
+    x = KL.Activation('relu')(x)
+
+    x = KL.Conv2D(nb_filter2, (kernel_size, kernel_size), padding='same',
+                  name=conv_name_base + '2b', use_bias=use_bias)(x)
+    x = BatchNorm(name=bn_name_base + '2b')(x, training=train_bn)
+    x = KL.Activation('relu')(x)
+
+    x = KL.Conv2D(nb_filter3, (1, 1), name=conv_name_base + '2c',
+                  use_bias=use_bias)(x)
+    x = BatchNorm(name=bn_name_base + '2c')(x, training=train_bn)
+
+    x = KL.Add()([x, input_tensor])
+    x = KL.Activation('relu', name='res' + str(stage) + block + '_out')(x)
+
+    return x
+
+
+def conv_block(input_tensor, kernel_size, filters, stage, block,
+               strides=(2, 2), use_bias=True, train_bn=True):
+    """conv_block is the block that has a conv layer at shortcut
+
+    Note that from stage 3, the first conv layer at main path is with
+    subsample=(2, 2) and the shortcut have subsmaple=(2, 2) as well
+    """
+    x = KL.Conv2D()
 
 
 
