3d030825d10d250298bf41c272221bc998061a34
Wubai Zhou <wubai.zhou@uber.com>
2018-06-17 15:44:42 -0700

    add cooccurence tfidf

--- src/relevance/features/gen_co_tfidf_feat.py
+++ src/relevance/features/gen_co_tfidf_feat.py
@@ -27,35 +27,29 @@ from relevance.utils.feat_utils import dump_feat_name
 from relevance.utils.nlp_utils import (
     stopwords, english_stemmer, stem_tokens, getTFV
 )
+from relevance.constants import token_pattern
 from relevance.config import config
 
-######################
-## Pre-process data ##
-######################
-token_pattern = r"(?u)\b\w\w+\b"
-#token_pattern = r'\w{1,}'
-#token_pattern = r"\w+"
-#token_pattern = r"[\w']+"
+
+# Pre-process data
 def preprocess_data(line,
                     token_pattern=token_pattern,
                     exclude_stopword=config.cooccurrence_word_exclude_stopword,
                     encode_digit=False):
-    token_pattern = re.compile(token_pattern, flags = re.UNICODE | re.LOCALE)
-    ## tokenize
+    token_pattern = re.compile(token_pattern, flags=re.UNICODE | re.LOCALE)
+    # tokenize
     tokens = [x.lower() for x in token_pattern.findall(line)]
-    ## stem
+    # stem
     tokens_stemmed = stem_tokens(tokens, english_stemmer)
     if exclude_stopword:
         tokens_stemmed = [x for x in tokens_stemmed if x not in stopwords]
     return tokens_stemmed
 
 
-########################
-## Cooccurrence terms ##
-########################
+# Cooccurrence terms
 def cooccurrence_terms(lst1, lst2, join_str):
     terms = [""] * len(lst1) * len(lst2)
-    cnt =  0
+    cnt = 0
     for item1 in lst1:
         for item2 in lst2:
             terms[cnt] = item1 + join_str + item2
@@ -64,53 +58,77 @@ def cooccurrence_terms(lst1, lst2, join_str):
     return res
 
 
-##################
-## Extract feat ##
-##################
+# Extract feat
 def extract_feat(df):
-    ## unigram
+    # unigram
     print "generate unigram"
     df["query_unigram"] = list(df.apply(lambda x: preprocess_data(x["query"]), axis=1))
     df["title_unigram"] = list(df.apply(lambda x: preprocess_data(x["product_title"]), axis=1))
     df["description_unigram"] = list(df.apply(lambda x: preprocess_data(x["product_description"]), axis=1))
-    ## bigram
+    # bigram
     print "generate bigram"
     join_str = "_"
     df["query_bigram"] = list(df.apply(lambda x: ngram.getBigram(x["query_unigram"], join_str), axis=1))
     df["title_bigram"] = list(df.apply(lambda x: ngram.getBigram(x["title_unigram"], join_str), axis=1))
     df["description_bigram"] = list(df.apply(lambda x: ngram.getBigram(x["description_unigram"], join_str), axis=1))
-    # ## trigram
-    # join_str = "_"
-    # df["query_trigram"] = list(df.apply(lambda x: ngram.getTrigram(x["query_unigram"], join_str), axis=1))
-    # df["title_trigram"] = list(df.apply(lambda x: ngram.getTrigram(x["title_unigram"], join_str), axis=1))
-    # df["description_trigram"] = list(df.apply(lambda x: ngram.getTrigram(x["description_unigram"], join_str), axis=1))
 
-    ## cooccurrence terms
+    # cooccurrence terms
     join_str = "X"
     # query unigram
-    df["query_unigram_title_unigram"] = list(df.apply(lambda x: cooccurrence_terms(x["query_unigram"], x["title_unigram"], join_str), axis=1))
-    df["query_unigram_title_bigram"] = list(df.apply(lambda x: cooccurrence_terms(x["query_unigram"], x["title_bigram"], join_str), axis=1))
-    df["query_unigram_description_unigram"] = list(df.apply(lambda x: cooccurrence_terms(x["query_unigram"], x["description_unigram"], join_str), axis=1))
-    df["query_unigram_description_bigram"] = list(df.apply(lambda x: cooccurrence_terms(x["query_unigram"], x["description_bigram"], join_str), axis=1))
+    df["query_unigram_title_unigram"] =\
+        list(df.apply(lambda x: cooccurrence_terms(x["query_unigram"],
+                                                   x["title_unigram"],
+                                                   join_str), axis=1))
+    df["query_unigram_title_bigram"] =\
+        list(df.apply(lambda x: cooccurrence_terms(x["query_unigram"],
+                                                   x["title_bigram"],
+                                                   join_str), axis=1))
+    df["query_unigram_description_unigram"] =\
+        list(df.apply(lambda x: cooccurrence_terms(x["query_unigram"],
+                                                   x["description_unigram"],
+                                                   join_str), axis=1))
+    df["query_unigram_description_bigram"] =\
+        list(df.apply(lambda x: cooccurrence_terms(x["query_unigram"],
+                                                   x["description_bigram"],
+                                                   join_str), axis=1))
     # query bigram
-    df["query_bigram_title_unigram"] = list(df.apply(lambda x: cooccurrence_terms(x["query_bigram"], x["title_unigram"], join_str), axis=1))
-    df["query_bigram_title_bigram"] = list(df.apply(lambda x: cooccurrence_terms(x["query_bigram"], x["title_bigram"], join_str), axis=1))
-    df["query_bigram_description_unigram"] = list(df.apply(lambda x: cooccurrence_terms(x["query_bigram"], x["description_unigram"], join_str), axis=1))
-    df["query_bigram_description_bigram"] = list(df.apply(lambda x: cooccurrence_terms(x["query_bigram"], x["description_bigram"], join_str), axis=1))
+    df["query_bigram_title_unigram"] =\
+        list(df.apply(lambda x: cooccurrence_terms(x["query_bigram"],
+                                                   x["title_unigram"],
+                                                   join_str), axis=1))
+    df["query_bigram_title_bigram"] =\
+        list(df.apply(lambda x: cooccurrence_terms(x["query_bigram"],
+                                                   x["title_bigram"],
+                                                   join_str), axis=1))
+    df["query_bigram_description_unigram"] =\
+        list(df.apply(lambda x: cooccurrence_terms(x["query_bigram"],
+                                                   x["description_unigram"],
+                                                   join_str), axis=1))
+    df["query_bigram_description_bigram"] =\
+        list(df.apply(lambda x: cooccurrence_terms(x["query_bigram"],
+                                                   x["description_bigram"],
+                                                   join_str), axis=1))
     # query id
-    df["query_id_title_unigram"] = list(df.apply(lambda x: cooccurrence_terms(["qid"+str(x["qid"])], x["title_unigram"], join_str), axis=1))
-    df["query_id_title_bigram"] = list(df.apply(lambda x: cooccurrence_terms(["qid"+str(x["qid"])], x["title_bigram"], join_str), axis=1))
-    df["query_id_description_unigram"] = list(df.apply(lambda x: cooccurrence_terms(["qid"+str(x["qid"])], x["description_unigram"], join_str), axis=1))
-    df["query_id_description_bigram"] = list(df.apply(lambda x: cooccurrence_terms(["qid"+str(x["qid"])], x["description_bigram"], join_str), axis=1))
-
+    df["query_id_title_unigram"] =\
+        list(df.apply(lambda x: cooccurrence_terms(["qid" + str(x["qid"])],
+                                                   x["title_unigram"],
+                                                   join_str), axis=1))
+    df["query_id_title_bigram"] =\
+        list(df.apply(lambda x: cooccurrence_terms(["qid" + str(x["qid"])],
+                                                   x["title_bigram"],
+                                                   join_str), axis=1))
+    df["query_id_description_unigram"] =\
+        list(df.apply(lambda x: cooccurrence_terms(["qid" + str(x["qid"])],
+                                                   x["description_unigram"],
+                                                   join_str), axis=1))
+    df["query_id_description_bigram"] =\
+        list(df.apply(lambda x: cooccurrence_terms(["qid" + str(x["qid"])],
+                                                   x["description_bigram"],
+                                                   join_str), axis=1))
 
 
 if __name__ == "__main__":
-
-    ############
-    ## Config ##
-    ############
-    ## cooccurrence terms column names
+    # cooccurrence terms column names
     column_names = [
         "query_unigram_title_unigram",
         "query_unigram_title_bigram",
@@ -125,51 +143,44 @@ if __name__ == "__main__":
         "query_id_description_unigram",
         "query_id_description_bigram",
     ]
-    ## feature names
-    feat_names = [ name+"_tfidf" for name in column_names ]
-    ## file to save feat names
+    # feature names
+    feat_names = [name + "_tfidf" for name in column_names]
+    # file to save feat names
     feat_name_file = "%s/intersect_tfidf.feat_name" % config.feat_folder
 
     ngram_range = config.cooccurrence_tfidf_ngram_range
 
     svd_n_components = 100
 
-    ###############
-    ## Load Data ##
-    ###############
-    ## load data
+    # load data
     with open(config.processed_train_data_path, "rb") as f:
         dfTrain = cPickle.load(f)
     with open(config.processed_test_data_path, "rb") as f:
         dfTest = cPickle.load(f)
-    ## load pre-defined stratified k-fold index
+    # load pre-defined stratified k-fold index
     with open("%s/stratifiedKFold.%s.pkl" % (config.data_folder, config.stratified_label), "rb") as f:
             skf = cPickle.load(f)
 
-    #######################
-    ## Generate Features ##
-    #######################
+    # Generate Features
     print("==================================================")
     print("Generate co-occurrence tfidf features...")
 
-    ## get cooccurrence terms
+    # get cooccurrence terms
     extract_feat(dfTrain)
     extract_feat(dfTest)
 
-    ######################
-    ## Cross validation ##
-    ######################
+    # Cross validation
     print("For cross-validation...")
     for run in range(config.n_runs):
-        ## use 33% for training and 67 % for validation
-        ## so we switch trainInd and validInd
+        # use 33% for training and 67 % for validation
+        # so we switch trainInd and validInd
         for fold, (validInd, trainInd) in enumerate(skf[run]):
-            print("Run: %d, Fold: %d" % (run+1, fold+1))
-            path = "%s/Run%d/Fold%d" % (config.feat_folder, run+1, fold+1)
+            print("Run: %d, Fold: %d" % (run + 1, fold + 1))
+            path = "%s/Run%d/Fold%d" % (config.feat_folder, run + 1, fold + 1)
 
-            for feat_name,column_name in zip(feat_names, column_names):
+            for feat_name, column_name in zip(feat_names, column_names):
                 print "generate %s feat" % feat_name
-                ## tfidf
+                # tfidf
                 tfv = getTFV(ngram_range=ngram_range)
                 X_tfidf_train = tfv.fit_transform(dfTrain.iloc[trainInd][column_name])
                 X_tfidf_valid = tfv.transform(dfTrain.iloc[validInd][column_name])
@@ -178,7 +189,7 @@ if __name__ == "__main__":
                 with open("%s/valid.%s.feat.pkl" % (path, feat_name), "wb") as f:
                     cPickle.dump(X_tfidf_valid, f, -1)
 
-                ## svd
+                # svd
                 svd = TruncatedSVD(n_components=svd_n_components, n_iter=15)
                 X_svd_train = svd.fit_transform(X_tfidf_train)
                 X_svd_test = svd.transform(X_tfidf_valid)
@@ -189,13 +200,10 @@ if __name__ == "__main__":
 
     print("Done.")
 
-
-    #################
-    ## Re-training ##
-    #################
+    # Re-training
     print("For training and testing...")
     path = "%s/All" % config.feat_folder
-    for feat_name,column_name in zip(feat_names, column_names):
+    for feat_name, column_name in zip(feat_names, column_names):
         print "generate %s feat" % feat_name
         tfv = getTFV(ngram_range=ngram_range)
         X_tfidf_train = tfv.fit_transform(dfTrain[column_name])
@@ -205,7 +213,7 @@ if __name__ == "__main__":
         with open("%s/test.%s.feat.pkl" % (path, feat_name), "wb") as f:
             cPickle.dump(X_tfidf_test, f, -1)
 
-        ## svd
+        # svd
         svd = TruncatedSVD(n_components=svd_n_components, n_iter=15)
         X_svd_train = svd.fit_transform(X_tfidf_train)
         X_svd_test = svd.transform(X_tfidf_test)
@@ -216,9 +224,10 @@ if __name__ == "__main__":
 
     print("Done.")
 
-    ## save feat names
+    # save feat names
     print("Feature names are stored in %s" % feat_name_file)
-    feat_names += [ "%s_individual_svd%d"%(f, svd_n_components) for f in feat_names ]
+    feat_names += ["%s_individual_svd%d" %
+                   (f, svd_n_components) for f in feat_names]
     dump_feat_name(feat_names, feat_name_file)
 
     print("All Done.")
